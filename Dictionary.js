11580dca6222a80e5977e2b6ad44eb37f4a9b91f
GMarc3l <105872971+GMarc3l@users.noreply.github.com>
2022-06-02 21:38:17 +0100

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -3,13 +3,21 @@ let dictionary = [];
 function addWord() {
     dictionary.push(document.getElementById("word").value);
     document.getElementById("word").value = '';
-    return document.getElementById("result").innerHTML = "Cuvantul a fost adaugat!"
+    setTimeout(() => {
+        const resultText = document.getElementById('result');
+        resultText.innerHTML = '';
+    }, 2000);
+    return document.getElementById("result").innerHTML = "Cuvantul a fost adaugat!";
 }
 
 
 function wordSearching() {
     var searchedWord = document.getElementById("search").value;
     document.getElementById("search").value='';
+    setTimeout(() => {
+        const resultText = document.getElementById('result');
+        resultText.innerHTML = '';
+    }, 2000);
     if (dictionary.indexOf(searchedWord) >= 0) {
         return document.getElementById("result").innerHTML = "Cuvantul este in dictionar!";
     }
